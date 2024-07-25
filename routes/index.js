3f9b263ad9613e645e40e93c3dc86cdd6223ccd7
Akshayreddyy <akshaykarasani69@gmail.com>
2024-07-25 15:57:58 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const ctrlMain = require('../controllers/main');
+const ctrlLocations =
+require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+/* Other pages */
+router.get('/about', ctrlOthers.about);
+/* GET homepage. */
+router.get('/', ctrlMain.index);
+module.exports = router;
